6e77ce5e778aba7d175ff12bcbe6ad0bd8718c51
Josef Dolezal <pepik.ml@gmail.com>
2017-05-31 19:32:15 +0200

    Create common error type

---
+++ Sources/IgnorioKit/IgnorioKitError.swift
@@ -0,0 +1,39 @@
+//
+//  IgnorioKitError.swift
+//  ignorio
+//
+//  Created by Josef Dolezal on 31/05/2017.
+//
+//
+
+import Foundation
+
+/// API requests error
+///
+/// - unknownTypeRequested: Thrown if user entered unknown type name
+/// - networkError: Trown when common network error occured
+/// - unexpectedResponseFormat: Thrown when API response does not have
+/// - requestTimeout: Thrown if the request is not completed in reasonable time
+public enum IgnorioKitError: Error, CustomStringConvertible {
+    case unknownTypeRequested(type: String)
+    case networkError(underlying: Error)
+    case unexpectedResponseFormat
+    case requestTimeout
+
+    /// String representation of error
+    public var description: String {
+        switch self {
+        case let .unknownTypeRequested(type):
+            return "The requested type '\(type)' is unknown."
+
+        case let .networkError(underlying):
+            return "An unknown error occured while performing network request: \(underlying.localizedDescription)"
+
+        case .unexpectedResponseFormat:
+            return "The format of server response does not have valid format."
+
+        case .requestTimeout:
+            return "The request timed out."
+        }
+    }
+}
