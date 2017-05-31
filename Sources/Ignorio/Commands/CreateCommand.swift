34c267440f0a47d789b13ffa26329fd0fe4fe7eb
Josef Dolezal <pepik.ml@gmail.com>
2017-05-31 19:34:21 +0200

    Create base API for gitignore fetch command

--- Sources/Ignorio/Commands/CreateCommand.swift
+++ Sources/Ignorio/Commands/CreateCommand.swift
@@ -6,12 +6,21 @@
 //
 //
 
+import Foundation
 import Commander
+import Result
 import IgnorioKit
 
 let createCommand = command(
     VariadicArgument<String>("TYPES", description: "Collection of types separated with space",
                              validator: typesValidator)
 ) { types in
-    
+    let ignorio = IgnorioKit()
+
+    let result = ignorio.create(types: types)
+
+    switch result {
+    case let .success(gitignore): print(gitignore)
+    case let .failure(error): throw error
+    }
 }
---
+++ Sources/IgnorioKit/IgnorioKit.swift
@@ -0,0 +1,70 @@
+//
+//  IgnorioKit.swift
+//  ignorio
+//
+//  Created by Josef Dolezal on 31/05/2017.
+//
+//
+
+import Foundation
+import Result
+
+/// Main logic for files retrieving
+public struct IgnorioKit {
+    /// Shortcut for result type
+    public typealias RequestResult = Result<String, IgnorioKitError>
+
+    /// Public accessible initializer
+    public init() { }
+
+    /// Requests .gitignore file content from API
+    ///
+    /// - Parameter types: List of types used to generate .gitignore
+    /// - Returns: The request result
+    public func create(types: [String]) -> RequestResult {
+        let result = synchronousRequest(GitignoreioAPI.create(types: types).request)
+
+        return result
+    }
+
+    /// Creates synchronuous URL request call
+    ///
+    /// - Parameter request: Request which will be called
+    /// - Returns: Network request result
+    private func synchronousRequest(_ request: URLRequest) -> RequestResult {
+        // Semaphore which makes the call synchronous
+        let semaphore = DispatchSemaphore(value: 0)
+        // Default result value
+        var result = RequestResult.failure(.requestTimeout)
+
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            // Create failure result with underlying error if occured
+            if let error = error {
+                result = .failure(IgnorioKitError.networkError(underlying: error))
+
+                semaphore.signal()
+                return
+            }
+
+            // Unwrap response text or create failure result
+            guard let data = data, let responseText = String(data: data, encoding: .utf8) else {
+                result = .failure(IgnorioKitError.unexpectedResponseFormat)
+
+                semaphore.signal()
+                return
+            }
+
+
+
+            // Create success result
+            result = .success(responseText)
+            semaphore.signal()
+        }
+
+        task.resume()
+        // Wait for request to complete (on the same thread)
+        _ = semaphore.wait(timeout: DispatchTime.distantFuture)
+        
+        return result
+    }
+}
